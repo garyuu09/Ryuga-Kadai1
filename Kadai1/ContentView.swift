46cb3bb2aba5b2ac715a4478d8673fbe3c6afce0
Ryuga <ribereo.minami@gmail.com>
2023-02-03 08:59:02 +0900

    5つのtextFieldの数字を足し算する機能

--- Kadai1/ContentView.swift
+++ Kadai1/ContentView.swift
@@ -10,33 +10,66 @@ import SwiftUI
 struct ContentView: View {
     @State var number1:Int = 0
     @State var number2:Int = 0
+    @State var number3:Int = 0
+    @State var number4:Int = 0
+    @State var number5:Int = 0
     @State var result = "Label"
 
 
     var body: some View {
-        VStack {
-            TextField("", value: $number1, formatter: NumberFormatter())
-                .font(.title)
-                .keyboardType(.numberPad)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-
-            TextField("", value: $number2, formatter: NumberFormatter())
-                .font(.title)
-                .keyboardType(.numberPad)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+        HStack{
+            VStack(alignment : .leading) {
+                TextField("", value: $number1, formatter: NumberFormatter())
+                    .font(.title)
+                    .frame(height:40)
+                    .frame(width: 100)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
 
 
-            Button(action: {
-                result = String(number1 + number2)
+                TextField("", value: $number2, formatter: NumberFormatter())
+                    .font(.title)
+                    .frame(height:40)
+                    .frame(width: 100)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                TextField("", value: $number3, formatter: NumberFormatter())
+                    .font(.title)
+                    .frame(height:40)
+                    .frame(width: 100)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                TextField("", value: $number4, formatter: NumberFormatter())
+                    .font(.title)
+                    .frame(height:40)
+                    .frame(width: 100)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                TextField("", value: $number5, formatter: NumberFormatter())
+                    .font(.title)
+                    .frame(height:40)
+                    .frame(width: 100)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+
+                Button(action: {
+                    result = String(number1 + number2 + number3 + number4 + number5)
 
                 }){
-                  Text("Button")
+                    Text("Button")
+                        .font(.title)
                 }
-            Text(result)
-
-
+                Text(result)
+                    .font(.title)
+                Spacer()
+            }
+            Spacer()
         }
-//        .padding()
+        .padding()
     }
 }
 
